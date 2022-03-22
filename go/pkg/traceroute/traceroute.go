netsec-ethz/scion

commit 70ed4a00c21d35ff6d7e6843c075c670f656a9f3

refactor: HopField and InfoField as value types
`HopField` and `InfoField` have been treated as pointer types, with functions like `slayers/path/scion.Raw.GetInfoField` and `GetHopField` returning newly allocated values. These functions are used frequently in the router dataplane and as such should be considered performance sensitive and allocations should generally be avoided.

These structs are small, `InfoField` is 8 bytes, `HopField` is (now) 14 bytes. Passing these as values causes no/little stack overhead compared to passing pointers.

The main changes in this PR are:
- change `HopField.Mac` from `[]byte` to `[6]byte`, allowing the `HopField` to be considered a value type and reducing the overall memory footprint
- change `Get`/`SetInfoField`  `Get`/`SetHopField` family of functions to return/accept values instead of pointers.
- generally pass `InfoField` and `HopField` as value, except where explicitly passed as mutable reference.
- change `slayers/path/scion.Decoded` `InfoFields` from `[]*path.InfoField` to `[]path.InfoField`, same for `HopFields`. This is not strictly necessary for the other changes, but it fits the theme. This should make creating `Decoded` paths more lightweight in terms of allocations (relevant in the router only for reversing paths for SCMPs).

Some tests needed to be adapted as they used an incorrect length for the hopfield MAC, which is no longer possible with the array type for the MAC.

Closes #4143

GitOrigin-RevId: ca1c018c8d51aacf6888c1dfbeea40999b825f27

go/pkg/traceroute/traceroute.go
@@ -142,7 +142,7 @@ func (t *tracerouter) Traceroute(ctx context.Context) (Stats, error) {
 	}()
 	prevXover := false
 	for i := 0; i < len(idxPath.HopFields); i++ {
-		hf := pktPath.HopFields[idxPath.PathMeta.CurrHF]
+		hf := &pktPath.HopFields[idxPath.PathMeta.CurrHF]
 		info := pktPath.InfoFields[idxPath.PathMeta.CurrINF]
 		// First hop of the path isn't probed, since only the egress hop is
 		// relevant.

go/cs/beacon/beacondbtest/BUILD.bazel
@@ -10,6 +10,7 @@ go_library(
         "//go/lib/addr:go_default_library",
         "//go/lib/common:go_default_library",
         "//go/lib/ctrl/seg:go_default_library",
+        "//go/lib/slayers/path:go_default_library",
         "//go/lib/xtest/graph:go_default_library",
         "@com_github_stretchr_testify//assert:go_default_library",
         "@com_github_stretchr_testify//require:go_default_library",

go/cs/beacon/beacondbtest/beacondbtest.go
@@ -15,7 +15,6 @@
 package beacondbtest
 
 import (
-	"bytes"
 	"context"
 	"testing"
 	"time"
@@ -27,6 +26,7 @@ import (
 	"github.com/scionproto/scion/go/lib/addr"
 	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/ctrl/seg"
+	"github.com/scionproto/scion/go/lib/slayers/path"
 	"github.com/scionproto/scion/go/lib/xtest/graph"
 )
 
@@ -369,7 +369,7 @@ func AllocBeacon(
 					ExpTime:     63,
 					ConsIngress: uint16(peer.Ingress),
 					ConsEgress:  uint16(as.Egress),
-					MAC:         bytes.Repeat([]byte{0xff}, 6),
+					MAC:         [path.MacLen]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
 				},
 			})
 		}
@@ -383,7 +383,7 @@ func AllocBeacon(
 					ExpTime:     63,
 					ConsIngress: uint16(as.Ingress),
 					ConsEgress:  uint16(as.Egress),
-					MAC:         bytes.Repeat([]byte{0xff}, 6),
+					MAC:         [path.MacLen]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
 				},
 			},
 			PeerEntries: peers,

go/cs/beaconing/extender.go
@@ -258,12 +258,14 @@ func (s *DefaultExtender) createHopF(ingress, egress uint16, ts time.Time,
 		panic(err)
 	}
 	fullMAC := mac.Sum(nil)
+	m := [path.MacLen]byte{}
+	copy(m[:], fullMAC[:path.MacLen])
 	return path.HopField{
 		ConsIngress: ingress,
 		ConsEgress:  egress,
 		ExpTime:     expTime,
-		Mac:         fullMAC[:6],
-	}, fullMAC[6:16]
+		Mac:         m,
+	}, fullMAC[path.MacLen:]
 }
 
 func extractBeta(pseg *seg.PathSegment) uint16 {

go/dispatcher/dispatcher/underlay_test.go
@@ -844,13 +844,16 @@ func TestSCMPHandlerReverse(t *testing.T) {
 							NumINF:  1,
 							NumHops: 3,
 						},
-						InfoFields: []*path.InfoField{
+						InfoFields: []path.InfoField{
 							{SegID: 0x111, ConsDir: true, Timestamp: 0x100},
 						},
-						HopFields: []*path.HopField{
-							{ConsIngress: 0, ConsEgress: 311, Mac: bytes.Repeat([]byte{0x00}, 6)},
-							{ConsIngress: 131, ConsEgress: 141, Mac: bytes.Repeat([]byte{0x01}, 6)},
-							{ConsIngress: 411, ConsEgress: 0, Mac: bytes.Repeat([]byte{0x02}, 6)},
+						HopFields: []path.HopField{
+							{ConsIngress: 0, ConsEgress: 311,
+								Mac: [path.MacLen]byte{0, 0, 0, 0, 0, 0}},
+							{ConsIngress: 131, ConsEgress: 141,
+								Mac: [path.MacLen]byte{1, 1, 1, 1, 1, 1}},
+							{ConsIngress: 411, ConsEgress: 0,
+								Mac: [path.MacLen]byte{2, 2, 2, 2, 2, 2}},
 						},
 					},
 				},
@@ -887,13 +890,16 @@ func TestSCMPHandlerReverse(t *testing.T) {
 							NumINF:  1,
 							NumHops: 3,
 						},
-						InfoFields: []*path.InfoField{
+						InfoFields: []path.InfoField{
 							{SegID: 0x111, ConsDir: false, Timestamp: 0x100},
 						},
-						HopFields: []*path.HopField{
-							{ConsIngress: 411, ConsEgress: 0, Mac: bytes.Repeat([]byte{0x02}, 6)},
-							{ConsIngress: 131, ConsEgress: 141, Mac: bytes.Repeat([]byte{0x01}, 6)},
-							{ConsIngress: 0, ConsEgress: 311, Mac: bytes.Repeat([]byte{0x00}, 6)},
+						HopFields: []path.HopField{
+							{ConsIngress: 411, ConsEgress: 0,
+								Mac: [path.MacLen]byte{2, 2, 2, 2, 2, 2}},
+							{ConsIngress: 131, ConsEgress: 141,
+								Mac: [path.MacLen]byte{1, 1, 1, 1, 1, 1}},
+							{ConsIngress: 0, ConsEgress: 311,
+								Mac: [path.MacLen]byte{0, 0, 0, 0, 0, 0}},
 						},
 					},
 				}
@@ -936,10 +942,10 @@ func newSCIONHdr(t *testing.T, l4 common.L4ProtocolType) *slayers.SCION {
 				NumINF:  1,
 				NumHops: 3,
 			},
-			InfoFields: []*path.InfoField{
+			InfoFields: []path.InfoField{
 				{SegID: 0x111, ConsDir: true, Timestamp: 0x100},
 			},
-			HopFields: []*path.HopField{
+			HopFields: []path.HopField{
 				{ConsIngress: 0, ConsEgress: 311},
 				{ConsIngress: 131, ConsEgress: 141},
 				{ConsIngress: 411, ConsEgress: 0},

go/dispatcher/internal/respool/packet_test.go
@@ -122,10 +122,10 @@ func scionLayer(t *testing.T, l4 common.L4ProtocolType) *slayers.SCION {
 				NumINF:  1,
 				NumHops: 3,
 			},
-			InfoFields: []*path.InfoField{
+			InfoFields: []path.InfoField{
 				{SegID: 0x111, ConsDir: true, Timestamp: 0x100},
 			},
-			HopFields: []*path.HopField{
+			HopFields: []path.HopField{
 				{ConsIngress: 0, ConsEgress: 311},
 				{ConsIngress: 131, ConsEgress: 141},
 				{ConsIngress: 411, ConsEgress: 0},

go/integration/braccept/cases/bfd.go
@@ -42,7 +42,9 @@ func bfdNormalizePacket(pkt gopacket.Packet) {
 				// Timestamps are generated by the sender from the current time.
 				p.Info.Timestamp = 0
 				// MACs are different because of different timestamps.
-				p.FirstHop.Mac = []byte{}
+				for i := range p.FirstHop.Mac {
+					p.FirstHop.Mac[i] = 0
+				}
 			}
 		case *layers.BFD:
 			// This field is randomly chosen by the sender.
@@ -90,7 +92,7 @@ func ExternalBFD(artifactsDir string, mac hash.Hash) runner.Case {
 			ConsEgress:  131,
 		},
 	}
-	ohp.FirstHop.Mac = path.MAC(mac, &ohp.Info, &ohp.FirstHop, nil)
+	ohp.FirstHop.Mac = path.MAC(mac, ohp.Info, ohp.FirstHop, nil)
 	scionL := &slayers.SCION{
 		Version:      0,
 		TrafficClass: 0xb8,

go/integration/braccept/cases/child_to_child_xover.go
@@ -72,7 +72,7 @@ func ChildToChildXover(artifactsDir string, mac hash.Hash) runner.Case {
 			NumINF:  2,
 			NumHops: 4,
 		},
-		InfoFields: []*path.InfoField{
+		InfoFields: []path.InfoField{
 			// up seg
 			{
 				SegID:     0x111,
@@ -86,7 +86,7 @@ func ChildToChildXover(artifactsDir string, mac hash.Hash) runner.Case {
 				Timestamp: util.TimeToSecs(time.Now()),
 			},
 		},
-		HopFields: []*path.HopField{
+		HopFields: []path.HopField{
 			{ConsIngress: 511, ConsEgress: 0},
 			{ConsIngress: 0, ConsEgress: 151},
 			{ConsIngress: 0, ConsEgress: 141},

commit 6b905f9df69ac8bafa107832a2b6b7b337529698

traceroute: fix setting router alert regression
Fix regression introduced in 70ed4a00c21d35ff6d7e6843c075c670f656a9f3.
This has already been fixed upstream in a7a1181011d26ab8157e7ab84375eac82719b25f.

go/pkg/traceroute/traceroute.go
@@ -189,7 +189,7 @@ func (t *tracerouter) probeHop(ctx context.Context, hfIdx uint8, egress bool) (U
 		return Update{}, serrors.WrapStr("decoding path", err)
 	}
 
-	hf := decoded.HopFields[hfIdx]
+	hf := &decoded.HopFields[hfIdx]
 	if egress {
 		hf.EgressRouterAlert = true
 	} else {