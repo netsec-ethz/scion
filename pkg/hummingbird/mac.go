82fc57fc9415d8fa72460539978884fd02657ff8
Jules Bachmann <julesb@student.ethz.ch>
2023-09-25 14:10:03 +0200

    updated ak and vk computation to new standard, updated tests to compare to library cmac and cbc implementations, updated runtime measurements to use go Benchmarking

--- pkg/hummingbird/mac.go
+++ pkg/hummingbird/mac.go
@@ -26,7 +26,7 @@ var ZeroBlock [aes.BlockSize]byte
 
 // Derive authentication key A_k
 // block is expected to be initialized beforehand with aes.NewCipher(sv), where sv is this AS' secret value
-func DeriveAuthKey(block cipher.Block, resId uint32, bw, in, eg, startTime, endTime uint16, buffer []byte) []byte {
+func DeriveAuthKey(block cipher.Block, resId uint32, bw, in, eg uint16, startTime uint32, resDuration uint16, buffer []byte) []byte {
 
 	if len(buffer) < BufferSize {
 		buffer = make([]byte, BufferSize)
@@ -37,9 +37,9 @@ func DeriveAuthKey(block cipher.Block, resId uint32, bw, in, eg, startTime, endT
 	buffer[3] = byte(bw)
 	binary.BigEndian.PutUint16(buffer[4:6], in)
 	binary.BigEndian.PutUint16(buffer[6:8], eg)
-	binary.BigEndian.PutUint16(buffer[8:10], startTime)
-	binary.BigEndian.PutUint16(buffer[10:12], endTime)
-	binary.BigEndian.PutUint32(buffer[12:16], 0) //padding
+	binary.BigEndian.PutUint32(buffer[8:12], startTime)
+	binary.BigEndian.PutUint16(buffer[12:14], resDuration)
+	binary.BigEndian.PutUint16(buffer[14:16], 0) //padding
 
 	// should xor input with iv, but we use iv = 0 => identity
 	block.Encrypt(buffer[0:16], buffer[0:16])
@@ -48,9 +48,12 @@ func DeriveAuthKey(block cipher.Block, resId uint32, bw, in, eg, startTime, endT
 
 // shifts left a 16 bytes array
 func shiftLeft(in []byte) {
-	flag := (in[8] & byte(128+64)) >> 6
-	binary.BigEndian.PutUint64(in[0:8], binary.BigEndian.Uint64(in[0:8])<<2+uint64(flag))
-	binary.BigEndian.PutUint64(in[8:16], binary.BigEndian.Uint64(in[8:16])<<2)
+	flag := (in[8]&byte(128))>>7 == 1
+	binary.BigEndian.PutUint64(in[0:8], binary.BigEndian.Uint64(in[0:8])<<1)
+	binary.BigEndian.PutUint64(in[8:16], binary.BigEndian.Uint64(in[8:16])<<1)
+	if flag {
+		in[7] |= 0x01
+	}
 }
 
 func xor(a, b []byte) {
@@ -61,10 +64,9 @@ func xor(a, b []byte) {
 // Computes full flyover mac vk
 // Needs a xkbuffer of 44 uint32s to store the expanded keys for aes
 // dummy buffer is memory used by key expansion to store decryption keys
-// TODO: remove usage of dummy buffer; is no longer used for AMD64 machines and should (not tested) be fine for arm64 machines as well
-func FullFlyoverMac(ak []byte, dstIA addr.IA, pktlen uint16, baseTime uint32, highResTime uint32, buffer []byte, xkbuffer []uint32) []byte {
-	if len(buffer) < 34 {
-		buffer = make([]byte, 34)
+func FullFlyoverMac(ak []byte, dstIA addr.IA, pktlen uint16, resStartTime uint16, highResTime uint32, buffer []byte, xkbuffer []uint32) []byte {
+	if len(buffer) < 32 {
+		buffer = make([]byte, 32)
 	}
 	if len(xkbuffer) < 44 {
 		xkbuffer = make([]uint32, 44)
@@ -72,34 +74,24 @@ func FullFlyoverMac(ak []byte, dstIA addr.IA, pktlen uint16, baseTime uint32, hi
 
 	binary.BigEndian.PutUint64(buffer[0:8], uint64(dstIA))
 	binary.BigEndian.PutUint16(buffer[8:10], pktlen)
-	binary.BigEndian.PutUint32(buffer[10:14], baseTime)
-	binary.BigEndian.PutUint32(buffer[14:18], highResTime)
+	binary.BigEndian.PutUint16(buffer[10:12], resStartTime)
+	binary.BigEndian.PutUint32(buffer[12:16], highResTime)
 
 	expandKeyAsm(10, &ak[0], &xkbuffer[0])
 	//compute subkeys
-	encryptBlockAsm(10, &xkbuffer[0], &buffer[18], &ZeroBlock[0])
+	encryptBlockAsm(10, &xkbuffer[0], &buffer[16], &ZeroBlock[0])
 
-	// Compute K2. Ignore K1 since we will always use K2
-	flag1 := buffer[18]&byte(128) == 0
-	flag2 := buffer[18]&byte(64) == 0
-	shiftLeft(buffer[18:34])
+	// Compute K1. Ignore K2 since we will always use K1
+	flag1 := buffer[16]&byte(128) == 0
+	shiftLeft(buffer[16:32])
 	if !flag1 {
-		buffer[32] ^= 0x01
-		buffer[33] ^= 0x0e
-	}
-	if !flag2 {
-		buffer[33] ^= 0x87
+		buffer[31] ^= 0x87
 	}
 	//Compute cmac
-	encryptBlockAsm(10, &xkbuffer[0], &buffer[0], &buffer[0])
-
-	buffer[0] ^= buffer[16]
-	buffer[1] ^= buffer[17]
-	xor(buffer[0:16], buffer[18:34])
-	buffer[2] ^= 0x80
+	xor(buffer[0:16], buffer[16:32])
 
 	encryptBlockAsm(10, &xkbuffer[0], &buffer[0], &buffer[0])
-	//TODO: return only first 28 bits (4 bytes, set last 4 to zero)
+
 	return buffer[0:16]
 }
 
--- pkg/hummingbird/mac_test.go
+++ pkg/hummingbird/mac_test.go
@@ -2,66 +2,80 @@ package hummingbird_test
 
 import (
 	"crypto/aes"
-	"fmt"
+	"crypto/cipher"
+	"encoding/binary"
 	"testing"
-	"time"
 
+	"github.com/dchest/cmac"
 	"github.com/scionproto/scion/pkg/addr"
 	"github.com/scionproto/scion/pkg/hummingbird"
 	"github.com/stretchr/testify/require"
 )
 
-// Example verified with https://cryptii.com/pipes/aes-encryption
 func TestDeriveAuthKey(t *testing.T) {
 	sv := []byte{0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7}
-	//resID_bw := []byte{0, 1, 2, 3}
 	var resId uint32 = 0x40
 	var bw uint16 = 0x0203
 	buffer := make([]byte, 16)
 	var in uint16 = 2
 	var eg uint16 = 5
-	var start uint16 = 0x0001
-	var end uint16 = 0x0203
-	expected := []byte{142, 19, 145, 119, 76, 2, 228, 18, 134, 111, 116, 45, 200, 172, 113, 219}
-	//8e 13 91 77 4c 02 e4 12 86 6f 74 2d c8 ac 71 db   a9 5a eb 01 10 5e b2 6d a2 7a 83 66 43 81 99 4f
-	//TODO: check 32 bytes buffer key result
+	var start uint32 = 0x0030001
+	var duration uint16 = 0x0203
+
 	block, err := aes.NewCipher(sv)
 	if err != nil {
 		require.Fail(t, err.Error())
 	}
 
-	key := hummingbird.DeriveAuthKey(block, resId, bw, in, eg, start, end, buffer)
+	// Compute expected result with library CBC
+	expected := make([]byte, 16)
+	binary.BigEndian.PutUint32(expected[0:4], resId<<10)
+	expected[2] |= byte(bw >> 8)
+	expected[3] = byte(bw)
+	binary.BigEndian.PutUint16(expected[4:6], in)
+	binary.BigEndian.PutUint16(expected[6:8], eg)
+	binary.BigEndian.PutUint32(expected[8:12], start)
+	binary.BigEndian.PutUint16(expected[12:14], duration)
+	binary.BigEndian.PutUint16(expected[14:16], 0)
+
+	var ZeroBlock [aes.BlockSize]byte
+	mode := cipher.NewCBCEncrypter(block, ZeroBlock[:])
+	mode.CryptBlocks(expected, expected)
+
+	// Run DeriveAuthKey Function
+	block, err = aes.NewCipher(sv)
+	if err != nil {
+		require.Fail(t, err.Error())
+	}
+
+	key := hummingbird.DeriveAuthKey(block, resId, bw, in, eg, start, duration, buffer)
 	require.Equal(t, expected, key)
 
-	key = hummingbird.DeriveAuthKey(block, resId, bw, in, eg, start, end, buffer)
+	key = hummingbird.DeriveAuthKey(block, resId, bw, in, eg, start, duration, buffer)
 	require.Equal(t, expected, key)
 }
 
-func TestMeasureDeriveAuthKey(t *testing.T) {
+func BenchmarkDeriveAuthKey(b *testing.B) {
 	sv := []byte{0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7}
-	//ca 25 microseconds
+
 	var resId uint32 = 0x40
 	var bw uint16 = 0x0203
 	buffer := make([]byte, 16)
 	var in uint16 = 2
 	var eg uint16 = 5
-	var startTime uint16 = 0x0001
-	var endTime uint16 = 0x0203
-	expected := []byte{142, 19, 145, 119, 76, 2, 228, 18, 134, 111, 116, 45, 200, 172, 113, 219}
+	var start uint32 = 0x0030001
+	var duration uint16 = 0x0203
 
 	block, err := aes.NewCipher(sv)
 	if err != nil {
-		require.Fail(t, err.Error())
+		require.Fail(b, err.Error())
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		hummingbird.DeriveAuthKey(block, resId, bw, in, eg, start, duration, buffer)
 	}
 
-	var key []byte
-	start := time.Now()
-	for i := 0; i < 1000; i++ {
-		key = hummingbird.DeriveAuthKey(block, resId, bw, in, eg, startTime, endTime, buffer)
-	}
-	elapsed := time.Since(start)
-	require.Equal(t, expected, key)
-	fmt.Print(elapsed)
 }
 
 // verified with https://artjomb.github.io/cryptojs-extension/
@@ -70,114 +84,52 @@ func TestFlyOverMac(t *testing.T) {
 	ak := []byte{142, 19, 145, 119, 76, 2, 228, 18, 134, 111, 116, 45, 200, 172, 113, 219}
 	var dstIA addr.IA = 326
 	var pktlen uint16 = 23
-	var baseTs uint32 = 1234
+	var resStartTs uint16 = 1234
 	var highResTs uint32 = 4321
-	buffer := make([]byte, 34)
+	buffer := make([]byte, 32)
 	xkbuffer := make([]uint32, 44)
-	expected := []byte{106, 137, 42, 100, 162, 8, 148, 176, 96, 188, 243, 236, 179, 195, 218, 185}
+
+	// Compute expected output based on library cmac implementation
+	expected := make([]byte, 16)
+	binary.BigEndian.PutUint64(expected[0:8], uint64(dstIA))
+	binary.BigEndian.PutUint16(expected[8:10], pktlen)
+	binary.BigEndian.PutUint16(expected[10:12], resStartTs)
+	binary.BigEndian.PutUint32(expected[12:16], highResTs)
+
+	block, err := aes.NewCipher(ak)
+	if err != nil {
+		require.Fail(t, err.Error())
+	}
+	c, err := cmac.New(block)
+	if err != nil {
+		require.Fail(t, err.Error())
+	}
+	if _, err := c.Write(expected[0:16]); err != nil {
+		require.Fail(t, err.Error())
+	}
+
+	expected = c.Sum(expected[:0])
+
 	//expected with 0, 23, 1234, 4321: 726f7d9e 17e3cbe1 d47a32eb d8a5e26e
-	mac := hummingbird.FullFlyoverMac(ak, dstIA, pktlen, baseTs, highResTs, buffer, xkbuffer)
+	mac := hummingbird.FullFlyoverMac(ak, dstIA, pktlen, resStartTs, highResTs, buffer, xkbuffer)
 	require.Equal(t, expected, mac)
-	mac = hummingbird.FullFlyoverMac(ak, dstIA, pktlen, baseTs, highResTs, buffer, xkbuffer)
+	mac = hummingbird.FullFlyoverMac(ak, dstIA, pktlen, resStartTs, highResTs, buffer, xkbuffer)
 	require.Equal(t, expected, mac)
 }
 
-// func TestMeasureFlyoverMac(t *testing.T) {
-// 	// ca 800 microseconds
-// 	ak := []byte{142, 19, 145, 119, 76, 2, 228, 18, 134, 111, 116, 45, 200, 172, 113, 219}
-// 	var dstIA addr.IA = 326
-// 	var pktlen uint16 = 23
-// 	var baseTs uint32 = 1234
-// 	var highResTs uint32 = 4321
-// 	buffer := make([]byte, 34)
-// 	expected := []byte{106, 137, 42, 100, 162, 8, 148, 176, 96, 188, 243, 236, 179, 195, 218, 185}
-
-// 	var mac []byte
-// 	var err error
-
-// 	start := time.Now()
-// 	for i := 0; i < 1000; i++ {
-// 		mac, err = hummingbird.FlyoverMac(ak, dstIA, pktlen, baseTs, highResTs, buffer)
-// 	}
-// 	elapsed := time.Since(start)
-// 	fmt.Print(elapsed)
-// 	require.Equal(t, expected, mac)
-// 	require.NoError(t, err)
-// }
-
-// func TestMeasureFlyoverMacSelfmade(t *testing.T) {
-// 	// ca 400 - 600 microseconds
-// 	ak := []byte{142, 19, 145, 119, 76, 2, 228, 18, 134, 111, 116, 45, 200, 172, 113, 219}
-// 	var dstIA addr.IA = 326
-// 	var pktlen uint16 = 23
-// 	var baseTs uint32 = 1234
-// 	var highResTs uint32 = 4321
-// 	buffer := make([]byte, 34)
-// 	expected := []byte{106, 137, 42, 100, 162, 8, 148, 176, 96, 188, 243, 236, 179, 195, 218, 185}
-
-// 	var mac []byte
-// 	var err error
-
-// 	start := time.Now()
-// 	for i := 0; i < 1000; i++ {
-// 		mac, err = hummingbird.FlyoverMacSelfmade(ak, dstIA, pktlen, baseTs, highResTs, buffer)
-// 	}
-// 	elapsed := time.Since(start)
-// 	fmt.Print(elapsed)
-// 	require.Equal(t, expected, mac)
-// 	require.NoError(t, err)
-// }
-
-// func TestMeasureFlyoverMacAes(t *testing.T) {
-// 	// ca 400 - 500 microseconds
-// 	// ca 530 - 750 microseconds with 256 bits key
-// 	// ca 90 microseconds with assembly aes
-// 	// ca 250 without key expansion
-// 	// ca 250-200 without aes.encrypt
-// 	// 12 with neither
-// 	ak := []byte{142, 19, 145, 119, 76, 2, 228, 18, 134, 111, 116, 45, 200, 172, 113, 219}
-// 	var dstIA addr.IA = 326
-// 	var pktlen uint16 = 23
-// 	var baseTs uint32 = 1234
-// 	var highResTs uint32 = 4321
-// 	buffer := make([]byte, 34)
-// 	xkbuffer := make([]uint32, 44)
-// 	dummy := make([]uint32, 44)
-// 	expected := []byte{106, 137, 42, 100, 162, 8, 148, 176, 96, 188, 243, 236, 179, 195, 218, 185}
-
-// 	var mac []byte
-// 	var err error
-
-// 	start := time.Now()
-// 	for i := 0; i < 1000; i++ {
-// 		mac, err = hummingbird.FlyoverMacAssembly(ak, dstIA, pktlen, baseTs, highResTs, buffer, xkbuffer, dummy)
-// 	}
-// 	elapsed := time.Since(start)
-// 	fmt.Print(elapsed)
-// 	require.Equal(t, expected, mac)
-// 	require.NoError(t, err)
-// }
-
-func TestMeasureFlyoverMac(t *testing.T) {
-	// ca 90 microseconds with assembly aes
+func BenchmarkFlyoverMac(b *testing.B) {
 	ak := []byte{142, 19, 145, 119, 76, 2, 228, 18, 134, 111, 116, 45, 200, 172, 113, 219}
 	var dstIA addr.IA = 326
 	var pktlen uint16 = 23
-	var baseTs uint32 = 1234
+	var resStartTs uint16 = 1234
 	var highResTs uint32 = 4321
-	buffer := make([]byte, 34)
+	buffer := make([]byte, 32)
 	xkbuffer := make([]uint32, 44)
-	expected := []byte{106, 137, 42, 100, 162, 8, 148, 176, 96, 188, 243, 236, 179, 195, 218, 185}
 
-	var mac []byte
-
-	start := time.Now()
-	for i := 0; i < 1000; i++ {
-		mac = hummingbird.FullFlyoverMac(ak, dstIA, pktlen, baseTs, highResTs, buffer, xkbuffer)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hummingbird.FullFlyoverMac(ak, dstIA, pktlen, resStartTs, highResTs, buffer, xkbuffer)
 	}
-	elapsed := time.Since(start)
-	fmt.Print(elapsed)
-	require.Equal(t, expected, mac)
 }
 
 func TestCompareAk(t *testing.T) {
@@ -191,38 +143,13 @@ func TestCompareAk(t *testing.T) {
 	require.False(t, hummingbird.CompareAk(a, c))
 }
 
-func TestMeasureCompareAk(t *testing.T) {
+func BenchmarkCompareAk(b *testing.B) {
 	a := []byte{142, 19, 145, 119, 76, 2, 228, 18, 134, 111, 116, 45, 200, 172, 113, 219}
-	b := []byte{142, 19, 145, 119, 76, 2, 228, 18, 134, 111, 116, 45, 200, 172, 113, 218}
-	res := true
-	start := time.Now()
-	for i := 0; i < 1000; i++ {
-		res = hummingbird.CompareAk(a, b)
+	c := []byte{142, 19, 145, 119, 76, 2, 228, 18, 134, 111, 116, 45, 200, 172, 113, 218}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hummingbird.CompareAk(a, c)
 	}
-	elapsed := time.Since(start)
-	fmt.Print(elapsed)
-	require.False(t, res)
-}
-
-// 480 ns
-func TestIntCompare(t *testing.T) {
-	var a uint64 = 128732
-	var b uint64 = 1287323
-	var c uint64 = 672964
-	var d uint64 = 672964
-	res := true
-	start := time.Now()
-	for i := 0; i < 1000; i++ {
-		res = CompareInt(a, b, c, d)
-	}
-	elapsed := time.Since(start)
-	fmt.Print(elapsed)
-	require.False(t, res)
-}
-
-// lower bound for 16 bytes comparison
-func CompareInt(a, b, c, d uint64) bool {
-	return a == b && c == d
 }
 
 func TestCompareVk(t *testing.T) {
@@ -252,15 +179,11 @@ func TestCompareVkPadded(t *testing.T) {
 	require.False(t, hummingbird.CompareVkPadded(e, g))
 }
 
-func TestMeasureCompareVk(t *testing.T) {
+func BenchmarkCompareVk(b *testing.B) {
 	a := []byte{1, 2, 3, 4}
-	b := []byte{1, 2, 4, 4}
-	res := true
-	start := time.Now()
-	for i := 0; i < 1000; i++ {
-		res = hummingbird.CompareVk(a, b)
+	c := []byte{1, 2, 4, 4}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hummingbird.CompareVk(a, c)
 	}
-	elapsed := time.Since(start)
-	fmt.Print(elapsed)
-	require.False(t, res)
 }
